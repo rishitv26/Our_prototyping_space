e63ed8d81df4c13790892fb2c4bd81d4c445f0a3
rishitv26 <75286874+rishitv26@users.noreply.github.com>
2021-01-29 13:58:19 -0500

    lol

---
+++ Rishit's_folder/CNN-list.py
@@ -0,0 +1,11 @@
+list = [
+    "salmon raising",
+    "farming in deserts",
+    "Inauguration",
+    'the halloween covid 19',
+    'wuhan china',
+    'covid vaccines',
+    'elections',
+]
+
+print(list)
