38c219ec056538ac935f0e297d8a88d420cfdcfe
rishitv26 <75286874+rishitv26@users.noreply.github.com>
2021-01-31 16:18:32 -0500

    example search

--- Rishit's_folder/search/Search.py
+++ Rishit's_folder/search/Search.py
@@ -1,5 +1,5 @@
 # create a list that will act like database:
-db = ['lololololololololololol']
+db = ['billy', 'sam', 'rishit', 'dhawan', 'ragav']
 
 # create an empty list to store filtered data:
 verified = []
@@ -8,18 +8,10 @@ run = True
 
 # here is the proccess:
 while run:
-    entered = input("> ")
-
-    for list in db:
-        processed = entered in db
-        if processed:
-            verified += list
-        if len(verified) == 0:
-            error = True
-            break
-    print(verified)
-
-
-if error:
-    print('no results...')
-    run = True
+    inputE = input('> ')
+    if inputE != '':
+        for i in db:
+            check = inputE in i
+            # print(check)
+            if check:
+                print(i)
